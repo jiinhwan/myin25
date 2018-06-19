5e85bfbc8ba6924926c0ffbe7e5b3169efd0c858
USER <USER@com201-09>
2018-06-19 14:06:35 +0900

    네번째라요

---
+++ SpringMyBatisNewBoard25/src/main/java/com/myin/team25/service/howhoho.java
@@ -0,0 +1,11 @@
+package com.myin.team25.service;
+
+public class howhoho {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("등등등등");
+	}
+
+}
